e5a42f86669f9d160f02e3acd1706278ab7f50ef
Alpha Zhou <zhouyuzhe88@163.com>
2018-11-18 20:15:43 +0800

    UI frame

---
+++ CloudDiskApp/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Common.Util;
+using System.Windows;
+
+namespace CloudDiskApp
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        public Client.Client Client { get; set; }
+
+        public App()
+        {
+            Client = new Client.Client();
+        }
+
+        private void SignIn()
+        {
+            string user = Settings.GetStringValue("currentUser");
+            string password = Settings.GetStringValue(string.Format("{0}.{1}", user, "password"));
+            Client.Signin(user, password, null);
+        }
+    }
+}
